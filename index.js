3b95bc3e5d3bd7851df98766022a6bbda5c6eaa4
informathemusic <39065949+informathemusic@users.noreply.github.com>
2019-09-08 15:31:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 let express=require('express');
 let http=require('http');
+let app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/dist/index.html');  
+})
